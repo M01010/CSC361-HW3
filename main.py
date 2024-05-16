3323ba754dd042583927489bd1f27c9fbd301bda
M01010 <ihaamoodi@gmail.com>
2024-05-17 02:40:54 +0300

    added bootstrap

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from flask import Flask, render_template, request
 from utils import load_pickle, get_prediction, load_json
 
 app = Flask(__name__)
-classification_pipelines: dict[str] = load_pickle('notebooks/classification/pipeline_models.pkl')
-classification_encoder = load_pickle('notebooks/classification/encoder.pkl')
-classification_json_data = load_json('notebooks/classification/column_data.json')
+
+classification_pipelines: dict[str] = load_pickle('notebooks/classification/files/pipeline_models.pkl')
+classification_encoder = load_pickle('notebooks/classification/files/encoder.pkl')
+classification_json_data = load_json('notebooks/classification/files/column_data.json')
 
 
 @app.route('/')
@@ -12,39 +13,26 @@ def index():
     return render_template('index.html')
 
 
-@app.get('/classification')
-def classification_get():
-    return render_template('classification.html',
-                           models=classification_pipelines.keys(),
-                           column_data=classification_json_data
-                           )
+@app.route('/classification', methods=['GET', 'POST'])
+def classification():
+    if request.method == 'GET':
+        return render_template('classification.html',
+                               models=classification_pipelines.keys(),
+                               column_data=classification_json_data)
+    else:
+        model, y_label = get_prediction(request.form, classification_pipelines, classification_encoder)
+        return render_template('classification.html',
+                               model=model,
+                               prediction=y_label)
 
 
-@app.post('/classification')
-def classification_post():
-    model, y_label = get_prediction(request.form, classification_pipelines, classification_encoder)
-    return render_template('classification.html',
-                           model=model,
-                           prediction=y_label)
-
-
-@app.get('/clustering')
-def clustering_get():
+@app.route('/clustering', methods=['GET', 'POST'])
+def clustering():
     return render_template('clustering.html')
 
 
-@app.post('/clustering')
-def clustering_post():
-    return render_template('clustering.html')
-
-
-@app.get('/regression')
-def regression_get():
-    return render_template('regression.html')
-
-
-@app.post('/regression')
-def regression_post():
+@app.route('/regression', methods=['GET', 'POST'])
+def regression():
     return render_template('regression.html')
 
 
